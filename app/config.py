7f226747c3db1b1e38cecff6336052677b466af1
Matteo Landi <matteo@matteolandi.net>
2013-06-01 10:30:14 +0200

    More config settings

--- app/config.py
+++ app/config.py
@@ -1,3 +1,14 @@
 
 APP_NAME = 'poolit'
 TAG = '0.0.1'
+
+debug = False
+debug_sql = False
+
+DEV = False
+
+LOGGER_NAME = APP_NAME
+LOG_ENABLE = True
+LOG_FORMAT = '[%(process)d] %(levelname)s %(message)s [in %(pathname)s:%(lineno)d]'
+
+DATABASE_URL = 'sqlite:///appdb.sqlite'
