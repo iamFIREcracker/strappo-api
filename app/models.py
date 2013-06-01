d31e69b834180e6b6987292783f6ea240289ce05
Matteo Landi <matteo@matteolandi.net>
2013-06-01 14:02:22 +0200

    Add user table

--- app/models.py
+++ app/models.py
@@ -23,3 +23,19 @@ class Session(Base):
     session_id = Column(String, primary_key=True)
     atime = Column(Time, default=datetime.now)
     data = Column(Text)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(String, default=uuid, primary_key=True)
+    created = Column(DateTime, default=datetime.now)
+    updated = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    avatar = Column(String, nullable=True)
+    deleted = Column(Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
