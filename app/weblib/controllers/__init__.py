a149e9abf7c8b1574c279aadc19f6bbe3633fe48
Matteo Landi <matteo@matteolandi.net>
2013-06-01 15:46:05 +0200

    Add a base controller to authorize requests using cookies

---
+++ app/weblib/controllers/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import abc
+
+import web
+
+
+class AbstractCookieAuthorizedController(object):
+    """
+    >>> class Handler(BaseHandler):
+    ...   def get_user(self, userid):
+    ...     return 'ok' if userid == 'valid' else 'no'
+
+    >>> web.cookies = lambda: dict()
+    >>> this = Handler()
+    >>> this.current_user
+    'no'
+
+    >>> web.cookies = lambda: dict(userid='valid')
+    >>> this = Handler()
+    >>> this.current_user
+    'ok'
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @property
+    def current_user(self):
+        if not hasattr(self, '_current_user'):
+            userid = web.cookies().get('userid')
+            self._current_user = self.get_user(userid)
+        return self._current_user
+
+    @abc.abstractmethod
+    def get_user(self, userid):
+        """Gets the user identified by ``userid`` or None otherwise."""
+        pass
