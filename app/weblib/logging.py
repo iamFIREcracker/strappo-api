987bf163c9596d3b6b76d47a74c00b68cd5b1588
Matteo Landi <matteo@matteolandi.net>
2013-06-01 14:08:26 +0200

    Add logging module to weblib

--- app/weblib/logging.py
+++ app/weblib/logging.py
@@ -13,36 +13,39 @@ Implements the logging support for Flask.
 
 from __future__ import absolute_import
 
+import web
+
 from logging import getLogger, StreamHandler, Formatter, getLoggerClass, DEBUG
 
 
-def create_logger(config):
-    """Creates a logger for the given application. This logger works
-similar to a regular Python logger but changes the effective logging
-level based on the application's debug flag. Furthermore this
-function also removes all attached handlers in case there was a
-logger with the log name before.
-"""
+def create_logger():
+    """Creates a logger for the given application.
+    
+    This logger works similar to a regular Python logger but changes the
+    effective logging level based on the application's debug flag.  Furthermore
+    this function also removes all attached handlers in case there was a logger
+    with the log name before.
+    """
     Logger = getLoggerClass()
 
     class DebugLogger(Logger):
         def getEffectiveLevel(x):
-            if x.level == 0 and config.debug:
+            if x.level == 0 and web.config.DEBUG:
                 return DEBUG
             return Logger.getEffectiveLevel(x)
 
     class DebugHandler(StreamHandler):
         def emit(x, record):
-            StreamHandler.emit(x, record) if config.debug else None
+            StreamHandler.emit(x, record) if web.config.DEBUG else None
 
     handler = DebugHandler()
     handler.setLevel(DEBUG)
-    handler.setFormatter(Formatter(config.LOG_FORMAT))
-    logger = getLogger(config.LOGGER_NAME)
+    handler.setFormatter(Formatter(web.config.LOG_FORMAT))
+    logger = getLogger(web.config.LOGGER_NAME)
     # just in case that was not a new logger, get rid of all the handlers
     # already attached to it.
     del logger.handlers[:]
     logger.__class__ = DebugLogger
-    if config.LOG_ENABLE:
+    if web.config.LOG_ENABLE:
         logger.addHandler(handler)
     return logger
