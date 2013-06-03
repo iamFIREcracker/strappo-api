6c5dc43f4338cef20d1d5eb06c9bfb336c317b37
Matteo Landi <matteo@matteolandi.net>
2013-06-03 07:55:29 +0200

    Add `with_account` method to users repository.

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from app.models import User
+from app.models import Account
 from app.models import Token
+from app.models import User
 
 
 class UsersRepository(object):
@@ -10,4 +11,11 @@ class UsersRepository(object):
     @staticmethod
     def authorized_by(token):
         return User.query.join(Token).filter(Token.id == token).\
+                filter(User.deleted == False).first()
+
+    @staticmethod
+    def with_account(external_id, account_type):
+        return User.query.join(Account).\
+                filter(Account.external_id == external_id).\
+                filter(Account.type == account_type).\
                 filter(User.deleted == False).first()
--- test/functional/repositories/test_users.py
+++ test/functional/repositories/test_users.py
@@ -3,6 +3,7 @@
 
 import unittest
 
+from app.models import Account
 from app.models import Token
 from app.models import User
 from app.repositories.users import UsersRepository
@@ -59,4 +60,49 @@ class TestUsersRepository(unittest.TestCase):
         # Then
         self.assertEquals('uid', user.id)
 
+    def test_having_account_with_invalid_external_id_should_return_nothing(self):
+        # When
+        user = UsersRepository.with_account('not_existing', 'facebook')
+
+        # Then
+        self.assertIsNone(user)
+
+    def test_having_account_with_invalid_account_type_should_return_nothing(self):
+        # Given
+        self.session.begin(subtransactions=True)
+        self.session.add(Account(id='aid', user_id='uid', external_id='eid',
+                         type='facebook'))
+        self.session.commit()
+        # When
+        user = UsersRepository.with_account('aid', 'twitter')
+
+        # Then
+        self.assertIsNone(user)
+
+    def test_having_account_pointing_to_a_deleted_user_should_return_nothing(self):
+        # Given
+        self.session.begin(subtransactions=True)
+        self.session.add(Account(id='aid', user_id='uid', external_id='eid',
+                         type='facebook'))
+        self.session.commit()
+        # When
+        user = UsersRepository.with_account('aid', 'facebook')
+
+        # Then
+        self.assertIsNone(user)
+
+    def test_having_account_with_valid_external_id_and_account_type_should_return_the_associated_user(self):
+        # Given
+        self.session.begin(subtransactions=True)
+        self.session.add(User(id='uid', name='Name', phone='Phone',
+                         avatar='Avatar'))
+        self.session.add(Account(id='aid', user_id='uid', external_id='eid',
+                         type='facebook'))
+        self.session.commit()
+
+        # When
+        user = UsersRepository.with_account('eid', 'facebook')
+
+        # Then
+        self.assertEquals('uid', user.id)
 
