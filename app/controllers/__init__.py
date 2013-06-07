5e5832a86d01886319625e2f41df83945e978e83
Matteo Landi <matteo@matteolandi.net>
2013-06-07 22:52:26 +0200

    Add implementations of controllers taking into account tokens

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from app.repositories.users import UsersRepository
+from app.weblib.controllers import AbstractCookieAuthorizedController
+from app.weblib.controllers import AbstractParamAuthorizedController
 
-class HelloHandler(object):
-    def GET(self):
-        return 'Hello world!'
+
+class CookieAuthorizedController(AbstractCookieAuthorizedController):
+    def get_user(self, token):
+        return UsersRepository.authorized_by(token)
+
+
+class ParamAuthorizedController(AbstractParamAuthorizedController):
+    def get_user(self, token):
+        return UsersRepository.authorized_by(token)
