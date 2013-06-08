8bb8b76bb332a7cd8206aab53f8cb3a46f0d87f4
Matteo Landi <matteo@matteolandi.net>
2013-06-08 08:21:54 +0200

    Cosmetic changes (add userscrores within name variables)

--- app/workflows/users.py
+++ app/workflows/users.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import app.forms.users as userforms
+import app.forms.users as user_forms
 from app.pubsub.users import AccountRefresher
 from app.pubsub.users import AlreadyRegisteredVerifier
 from app.pubsub.users import TokenRefresher
@@ -17,61 +17,61 @@ from app.weblib.pubsub.auth import InSessionVerifier
 class LoginAuthorizedWorkflow(Publisher):
     """Defines a workflow managing the user post-authorization process."""
 
-    def perform(self, logger, session, sessionkey, paramsextractor,
+    def perform(self, logger, session, session_key, params_extractor,
                 repository, account_type):
         outer = self # Handy to access ``self`` from inner classes
         logger = LoggingSubscriber(logger)
-        sessionverifier = InSessionVerifier()
-        alreadyregistered = AlreadyRegisteredVerifier()
-        formvalidator = FormValidator()
-        usercreator = UserCreator()
-        accountrefresher = AccountRefresher()
-        tokenrefresher = TokenRefresher()
+        session_verifier = InSessionVerifier()
+        already_registered = AlreadyRegisteredVerifier()
+        form_validator = FormValidator()
+        user_creator = UserCreator()
+        account_refresher = AccountRefresher()
+        token_refresher = TokenRefresher()
         params = Future()
-        future_userid = Future()
+        future_user_id = Future()
 
         class InSessionVerifierSubscriber(object):
             def session_lacks(self, key):
                 outer.publish('not_authorized')
             def session_contains(self, key, value):
-                params.set(paramsextractor(value))
-                alreadyregistered.perform(repository, params.get().externalid,
-                                          account_type)
+                params.set(params_extractor(value))
+                already_registered.perform(repository, params.get().externalid,
+                                           account_type)
 
         class AlreadyRegisteredVerifierSubscriber(object):
-            def not_registered(self, externalid, account_type):
-                formvalidator.perform(userforms.add(), params.get(),
-                                      describe_invalid_form)
-            def already_registered(self, userid):
-                future_userid.set(userid)
-                tokenrefresher.perform(repository, userid)
+            def not_registered(self, external_id, account_type):
+                form_validator.perform(user_forms.add(), params.get(),
+                                       describe_invalid_form)
+            def already_registered(self, user_id):
+                future_user_id.set(user_id)
+                token_refresher.perform(repository, user_id)
 
         class FormValidatorSubscriber(object):
             def invalid_form(self, errors):
                 outer.publish('invalid_form',
                               dict(success=False, errors=errors))
             def valid_form(self, form):
-                usercreator.perform(repository, form.d.name, form.d.avatar)
+                user_creator.perform(repository, form.d.name, form.d.avatar)
 
         class UserCreatorSubscriber(object):
-            def user_created(self, userid, name, avatar):
-                future_userid.set(userid)
-                accountrefresher.perform(repository, userid,
-                                         params.get().externalid, account_type)
+            def user_created(self, user_id, name, avatar):
+                future_user_id.set(user_id)
+                account_refresher.perform(repository, user_id,
+                                          params.get().externalid, account_type)
 
         class AccountRefresherSubscriber(object):
-            def account_refreshed(self, accountid):
-                tokenrefresher.perform(repository, future_userid.get())
+            def account_refreshed(self, account_id):
+                token_refresher.perform(repository, future_user_id.get())
 
         class TokenRefresherSubscriber(object):
-            def token_refreshed(self, tokenid):
-                outer.publish('token_created', tokenid)
+            def token_refreshed(self, token_id):
+                outer.publish('token_created', token_id)
 
-        sessionverifier.add_subscriber(logger, InSessionVerifierSubscriber())
-        alreadyregistered.add_subscriber(logger,
-                                         AlreadyRegisteredVerifierSubscriber())
-        formvalidator.add_subscriber(logger, FormValidatorSubscriber())
-        usercreator.add_subscriber(logger, UserCreatorSubscriber())
-        accountrefresher.add_subscriber(logger, AccountRefresherSubscriber())
-        tokenrefresher.add_subscriber(logger, TokenRefresherSubscriber())
-        sessionverifier.perform(session, sessionkey)
+        session_verifier.add_subscriber(logger, InSessionVerifierSubscriber())
+        already_registered.add_subscriber(logger,
+                                          AlreadyRegisteredVerifierSubscriber())
+        form_validator.add_subscriber(logger, FormValidatorSubscriber())
+        user_creator.add_subscriber(logger, UserCreatorSubscriber())
+        account_refresher.add_subscriber(logger, AccountRefresherSubscriber())
+        token_refresher.add_subscriber(logger, TokenRefresherSubscriber())
+        session_verifier.perform(session, session_key)
