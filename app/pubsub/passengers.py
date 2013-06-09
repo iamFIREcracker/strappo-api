63ca119c037ee11d74ad38b102beb42a6c080c5f
Matteo Landi <matteo@matteolandi.net>
2013-06-09 15:23:03 +0200

    Add a passenger-with-id getter and a single passenger serializer

--- app/pubsub/passengers.py
+++ app/pubsub/passengers.py
@@ -4,6 +4,22 @@
 from app.weblib.pubsub import Publisher
 
 
+class PassengerWithIdGetter(Publisher):
+    def perform(self, repository, passenger_id):
+        """Get the passenger identified by ``passenger_id``.
+
+        If such passenger exists, a 'passenger_found' message is published
+        containing the passenger details;  on the other hand, if no passenger
+        exists with the specified ID, a 'passenger_not_found' message will be
+        published
+        """
+        passenger = repository.get(passenger_id)
+        if passenger is None:
+            self.publish('passenger_not_found', passenger_id)
+        else:
+            self.publish('passenger_found', passenger)
+
+
 class AllPassengersGetter(Publisher):
     def perform(self, repository):
         """Search for all the active passengers around.
@@ -14,6 +30,23 @@ class AllPassengersGetter(Publisher):
         self.publish('passengers_found', repository.get_all())
 
 
+def _serialize(p):
+    return dict(id=p.id, user_id=p.user_id, origin=p.origin,
+                destination=p.destination, buddies=p.buddies, name=p.user.name,
+                avatar=p.user.avatar)
+
+
+class PassengerSerializer(Publisher):
+    def perform(self, passenger):
+        """Convert the given passenger into a serializable dictionary.
+
+        At the end of the operation the method will emit a
+        'passenger_serialized' message containing the serialized object (i.e.
+        passenger dictionary).
+        """
+        self.publish('passenger_serialized', _serialize(passenger))
+
+
 class MultiplePassengersSerializer(Publisher):
     def perform(self, passengers):
         """Convert a list of passengers into serializable dictionaries.
@@ -22,7 +55,4 @@ class MultiplePassengersSerializer(Publisher):
         'passengers_serialized' message containing serialized objects.
         """
         self.publish('passengers_serialized',
-                     [dict(id=p.id, user_id=p.user_id, origin=p.origin,
-                           destination=p.destination, buddies=p.buddies,
-                           name=p.user.name, avatar=p.user.avatar)
-                      for p in passengers])
+                     [_serialize(p) for p in passengers])
