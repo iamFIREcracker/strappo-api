1ab3fadb25d9129c24b62586480e6d31b10ec47b
Matteo Landi <matteo@matteolandi.net>
2013-06-11 07:37:53 +0200

    Add form to validate the creation of a new passenger

---
+++ app/forms/passengers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from web import form
+
+
+__all__ = ['add']
+
+
+validint = form.Validator('Should be an integer', int)
+
+
+add = form.Form(form.Textbox('origin', form.notnull, description='Origin'),
+                form.Textbox('destination', form.notnull,
+                             description='Destination'),
+                form.Textbox('buddies', form.notnull, description='Buddies'))
