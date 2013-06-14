1f9fdf4fbea46cfa80934edb5701e3f3a839a164
Matteo Landi <matteo@matteolandi.net>
2013-06-14 07:59:29 +0200

    Add destinations workflow test for predefined destinations

--- test/integration/workflows/test_destinations.py
+++ test/integration/workflows/test_destinations.py
@@ -42,3 +42,28 @@ class TestListDestinationsWorkflow(unittest.TestCase):
             'dest2',
             'dest3'
         ])
+
+    def test_get_predefined_destinations_should_return_hardcoded_ones(self):
+        # Given
+        logger = Mock()
+        destinations = [('Darsena', 10),
+                        ('Mojito Bar', 10),
+                        ('Cosmopolitan', 10),
+                        ('Red Lion', 10),
+                        ('Club Negroni', 10)]
+        repository = Mock(get_predefined=MagicMock(return_value=destinations))
+        subscriber = Mock(success=MagicMock())
+        instance = ListDestinationsWorkflow()
+
+        # When
+        instance.add_subscriber(subscriber)
+        instance.perform(logger, repository.get_predefined)
+
+        # Then
+        subscriber.success.assert_called_with([
+            'Darsena',
+            'Mojito Bar',
+            'Cosmopolitan',
+            'Red Lion', 
+            'Club Negroni'
+        ])
