bbc7def2e1d00ba8a3ad286dfad3b3aca705f6f0
Matteo Landi <matteo@matteolandi.net>
2013-06-24 19:24:07 +0200

    Add passengers repository method to fetch all the active and accepted passengers, associated with a given driver id.

--- app/repositories/passengers.py
+++ app/repositories/passengers.py
@@ -4,6 +4,7 @@
 import uuid
 
 from app.models import Passenger
+from app.models import RideRequest
 from app.models import User
 from app.weblib.db import expunged
 
@@ -22,6 +23,23 @@ class PassengersRepository(object):
                 for p in Passenger.query.join(User).\
                         filter(User.deleted == False).\
                         filter(Passenger.active == True)]
+
+    @staticmethod
+    def get_all_accepted_by_driver(driver_id):
+        """Returns all the _accepted_ passengers, currently in _active_ state.
+
+        A passenger is considered _accepted_ if a ``RideRequest`` exists with
+        'accepted' equal to ``True`` and 'passenger_id' equal to the passenger
+        ID;  on the other hand, a passenger is marked as active if its 'active'
+        property is equal to ``True`` (i.e. it is still the same day in which
+        the passenger record has been added to the system.
+        """
+        return [expunged(p, Passenger.session)
+                for p in Passenger.query.join(User).join(RideRequest).\
+                        filter(User.deleted == False).\
+                        filter(Passenger.active == True).\
+                        filter(RideRequest.driver_id == driver_id).\
+                        filter(RideRequest.accepted == True)]
 
     @staticmethod
     def add(user_id, origin, destination, buddies):
