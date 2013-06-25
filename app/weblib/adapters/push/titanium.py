f92fc26243089c33cd8be3f79dffe495d1b72f8f
Matteo Landi <matteo@matteolandi.net>
2013-06-25 23:22:51 +0200

    Add an adapater for the titanium push notification API (only the notify_tokens endpoint has been wrapped)

---
+++ app/weblib/adapters/push/titanium.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import urllib
+import urllib2
+
+import web
+
+
+class TitaniumPushNotificationsAdapter(object):
+    """Defines an adapter of the Titanium push notification system."""
+
+    NOTIFY_TOKENS_URL = 'https://api.cloud.appcelerator.com/v1/push_notification/notify_tokens.json'
+
+    def notify_tokens(self, channel, tokens, payload):
+        data = urllib.urlencode(dict(key=web.config.TITANIUM_KEY,
+                                     channel=channel,
+                                     to_tokens=','.join(tokens),
+                                     payload=payload))
+        req = urllib2.request(self.NOTIFY_TOKENS_URL, data)
+        response = urllib2.urlopen(req)
+        try:
+            payload = json.load(response)
+            if payload['meta']['status'] == 'ok':
+                return (None, None)
+        except Exception as e:
+            return (None, e)
+
