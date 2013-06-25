2d885059b7f2f261ed50465c5a32fa3ec62ae7fa
Matteo Landi <matteo@matteolandi.net>
2013-06-25 23:23:12 +0200

    Add task to manage drivers notification

--- app/tasks.py
+++ app/tasks.py
@@ -3,7 +3,31 @@
 
 from app.celery import celery
 
+from app.weblib.pubsub import Future
+from app.repositories.drivers import DriversRepository
+from app.weblib.logger import create_logger
+from app.weblib.pubsub import LoggingSubscriber
+from app.weblib.adapters.push.titanium import TitaniumPushNotificationsAdapter
+from app.workflows.drivers import NotifyDriversWorkflow
+
 
 @celery.task
-def add(x, y):
-    return x + y
+class NotifyDriversTask(object):
+    def __call__(self, passenger_id):
+        logger = create_logger()
+        logging_subscriber = LoggingSubscriber(logger)
+        push_adapter = TitaniumPushNotificationsAdapter()
+        notify_drivers = NotifyDriversWorkflow()
+        ret = Future()
+
+        class NotifyDriversSubscriber(object):
+            def failure(self, error):
+                ret.set((None, error))
+            def success(self):
+                ret.set((None, None))
+
+        notify_drivers.add_subscriber(logging_subscriber,
+                                      NotifyDriversSubscriber())
+        notify_drivers.perform(logger, DriversRepository, passenger_id,
+                               push_adapter)
+        return ret.get()
