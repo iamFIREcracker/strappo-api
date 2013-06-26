6fe8bd605130b8ae871b8f04661284f35bb27e20
Matteo Landi <matteo@matteolandi.net>
2013-06-26 07:12:50 +0200

    Use functions for tasks.
    I didn't find out a way to use class members as tasks without having to specify
    the name of the task.

--- app/tasks.py
+++ app/tasks.py
@@ -3,31 +3,30 @@
 
 from app.celery import celery
 
+from app.weblib.logging import create_logger
 from app.weblib.pubsub import Future
 from app.repositories.drivers import DriversRepository
-from app.weblib.logger import create_logger
 from app.weblib.pubsub import LoggingSubscriber
 from app.weblib.adapters.push.titanium import TitaniumPushNotificationsAdapter
 from app.workflows.drivers import NotifyDriversWorkflow
 
 
 @celery.task
-class NotifyDriversTask(object):
-    def __call__(self, passenger_id):
-        logger = create_logger()
-        logging_subscriber = LoggingSubscriber(logger)
-        push_adapter = TitaniumPushNotificationsAdapter()
-        notify_drivers = NotifyDriversWorkflow()
-        ret = Future()
+def NotifyDriversTask(passenger_id):
+    logger = create_logger()
+    logging_subscriber = LoggingSubscriber(logger)
+    push_adapter = TitaniumPushNotificationsAdapter()
+    notify_drivers = NotifyDriversWorkflow()
+    ret = Future()
 
-        class NotifyDriversSubscriber(object):
-            def failure(self, error):
-                ret.set((None, error))
-            def success(self):
-                ret.set((None, None))
+    class NotifyDriversSubscriber(object):
+        def failure(self, error):
+            ret.set((None, error))
+        def success(self):
+            ret.set((None, None))
 
-        notify_drivers.add_subscriber(logging_subscriber,
-                                      NotifyDriversSubscriber())
-        notify_drivers.perform(logger, DriversRepository, passenger_id,
-                               push_adapter)
-        return ret.get()
+    notify_drivers.add_subscriber(logging_subscriber,
+                                    NotifyDriversSubscriber())
+    notify_drivers.perform(logger, DriversRepository, push_adapter,
+                           'drivers', passenger_id) # XXX TBD payload
+    return ret.get()
