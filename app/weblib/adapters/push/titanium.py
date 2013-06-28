e3425793b0f8cc08cf116353e82e875a909da7f6
Matteo Landi <matteo@matteolandi.net>
2013-06-28 07:03:37 +0200

    Refactor titanium push_notifications adapter

--- app/weblib/adapters/push/titanium.py
+++ app/weblib/adapters/push/titanium.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 import urllib
 import urllib2
 
@@ -11,23 +10,16 @@ import web
 class TitaniumPushNotificationsAdapter(object):
     """Defines an adapter of the Titanium push notification system."""
 
-    NOTIFY_TOKENS_URL = 'https://api.cloud.appcelerator.com/v1/push_notification/notify_tokens.json'
+    NOTIFY_TOKENS_URL = 'https://api.cloud.appcelerator.com/v1/push_notification/notify_tokens.json?key=%(key)s'
 
     def notify_tokens(self, channel, tokens, payload):
-        data = urllib.urlencode(dict(key=web.config.TITANIUM_KEY,
-                                     channel=channel,
+        url = self.NOTIFY_TOKENS_URL % dict(key=web.config.TITANIUM_KEY)
+        data = urllib.urlencode(dict(channel=channel,
                                      to_tokens=','.join(tokens),
                                      payload=payload))
-        req = urllib2.Request(self.NOTIFY_TOKENS_URL, data)
         try:
-            response = urllib2.urlopen(req)
+            response = urllib2.urlopen(url, data)
         except urllib2.HTTPError as e:
-            return (None, ('Unable to contact the server',
-                           self.NOTIFY_TOKENS_URL, data))
-        try:
-            payload = json.load(response)
-            if payload['meta']['status'] == 'ok':
-                return (None, None)
-        except Exception as e:
-            return (None, e)
-
+            return (None, ('Unable to contact the server', url, data, str(e)))
+        else:
+            return (response, None)
