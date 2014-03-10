76eab38a877dced58c0df386ca58fb502d9f8b77
Matteo Landi <matteo@matteolandi.net>
2014-03-10 01:17:49 +0100

    Disable periodic tasks

--- celeryconfig.py
+++ celeryconfig.py
@@ -8,17 +8,17 @@ CELERY_IMPORTS = ('app.tasks',)
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-CELERYBEAT_SCHEDULE = {
-    'deactivate-passengers-every-morning': {
-        'task': 'app.tasks.DeactivateActivePassengers',
-        'schedule': crontab(hour=5, minute=0)
-    },
-    'deactivate-drive-requests-every-morning': {
-        'task': 'app.tasks.DeactivateActiveDriveRequests',
-        'schedule': crontab(hour=5, minute=5)
-    },
-    'unhide-hidden-drivers-every-morning': {
-        'task': 'app.tasks.UnhideHiddenDrivers',
-        'schedule': crontab(hour=5, minute=10)
-    }
-}
+#CELERYBEAT_SCHEDULE = {
+    #'deactivate-passengers-every-morning': {
+        #'task': 'app.tasks.DeactivateActivePassengers',
+        #'schedule': crontab(hour=5, minute=0)
+    #},
+    #'deactivate-drive-requests-every-morning': {
+        #'task': 'app.tasks.DeactivateActiveDriveRequests',
+        #'schedule': crontab(hour=5, minute=5)
+    #},
+    #'unhide-hidden-drivers-every-morning': {
+        #'task': 'app.tasks.UnhideHiddenDrivers',
+        #'schedule': crontab(hour=5, minute=10)
+    #}
+#}
