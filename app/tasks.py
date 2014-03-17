57230b81361843b354eb4db42571ace9d7b15180
Matteo Landi <matteo@matteolandi.net>
2014-03-17 23:41:48 +0100

    `fortune`

--- app/tasks.py
+++ app/tasks.py
@@ -177,7 +177,7 @@ def NotifyPassengerDriveRequestPending(request):
                                   'drive_request': request,
                                   'alert': 'Yeah, %(name)s has offered '
                                            'to give you a ride!' \
-                                                   % dict(name=request['driver']['user']['name'])
+                                                   % dict(name=request['request']['user']['name'])
                               }))
     return ret.get()
 
