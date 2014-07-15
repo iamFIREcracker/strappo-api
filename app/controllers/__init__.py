f28a16b22fa425fb342301d866fe4a54bd1d26cc
Matteo Landi <matteo@matteolandi.net>
2014-07-15 19:57:24 +0200

    Return the minimum supported mobile version and the list of served regions

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -6,6 +6,7 @@ import web
 from app.repositories.users import UsersRepository
 from app.weblib.controllers import AbstractCookieAuthorizableController
 from app.weblib.controllers import AbstractParamAuthorizableController
+from app.weblib.utils import jsonify
 
 
 class CookieAuthorizableController(AbstractCookieAuthorizableController):
@@ -20,4 +21,31 @@ class ParamAuthorizableController(AbstractParamAuthorizableController):
 
 class InfoController(object):
     def GET(self):
-        return ""
+        return jsonify({
+            'min_version':'0.0.1',
+            'served_regions':[
+                {
+                    'name': 'Viareggio',
+                    'center': {
+                        'lat': 43.873676,
+                        'lon': 10.248534
+                    },
+                    'radius': 10,
+                    'hours': [
+                         {
+                             'day_of_week': 0,
+                             'from': 16,
+                             'to': 24
+                         }, {
+                             'day_of_week': 2,
+                             'from': 18,
+                             'to': 2
+                         }, {
+                             'day_of_week': 6,
+                             'from': 18,
+                             'to': 2
+                         }
+                    ]
+                }
+            ]
+        })
