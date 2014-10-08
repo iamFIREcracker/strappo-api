31bc90cb9c018c88bfdcf754da8072b1cae8b8fb
Matteo Landi <matteo@matteolandi.net>
2014-10-08 11:34:40 -0400

    Add ReprMixin class to automatically print SQLAlchemy models

--- app/models.py
+++ app/models.py
@@ -17,13 +17,14 @@ from app.weblib.db import String
 from app.weblib.db import Text
 from app.weblib.db import Time
 from app.weblib.db import text
+from app.weblib.db import ReprMixin
 
 
 
 Base = declarative_base()
 
 
-class User(Base):
+class User(Base, ReprMixin):
     __tablename__ = 'user'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -47,15 +48,8 @@ class User(Base):
                      primaryjoin="and_(User.id == Passenger.user_id,"
                                       "Passenger.active == True)")
 
-    def __repr__(self):
-        data = u'<User id=%(id)s, acs_id=%(acs_id)s, '\
-                'facebook_id=%(facebook_id)s, name=%(name)s, '\
-                'avatar=%(avatar)s, email=%(email)s, '\
-                'locale=%(locale)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Token(Base):
+class Token(Base, ReprMixin):
     __tablename__ = 'token'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -64,12 +58,8 @@ class Token(Base):
     updated = Column(DateTime, default=datetime.utcnow,
                      onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        data = u'<Token id=%(id)s, user_id=%(user_id)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Driver(Base):
+class Driver(Base, ReprMixin):
     __tablename__ = 'driver'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -90,19 +80,8 @@ class Driver(Base):
                                   primaryjoin="and_(Driver.id == DriveRequest.driver_id,"
                                                    "DriveRequest.active == True)")
 
-    def __repr__(self):
-        data = u'<Driver id=%(id)s, '\
-                'user_id=%(user_id)s, '\
-                'car_make=%(car_make)s, '\
-                'car_model=%(car_model)s, '\
-                'car_color=%(car_color)s, '\
-                'license_plate=%(license_plate)s, '\
-                'telephone=%(telephone)s, hidden=%(hidden)s, '\
-                'active=%(active)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Passenger(Base):
+class Passenger(Base, ReprMixin):
     __tablename__ = 'passenger'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -126,18 +105,8 @@ class Passenger(Base):
                                   primaryjoin="and_(Passenger.id == DriveRequest.passenger_id,"
                                                    "DriveRequest.active == True)")
 
-    def __repr__(self):
-        data = u'<Passenger id=%(id)s, user_id=%(user_id)s, '\
-               'origin=%(origin)s, origin_latitude=%(origin_latitude)s, '\
-               'origin_longitude=%(origin_longitude)s, '\
-               'destination=%(destination)s, destination_latitude=%(destination_latitude)s, '\
-               'destination_longitude=%(destination_longitude)s, '\
-               'distance=%(distance)s, seats=%(seats)s, '\
-               'matched=%(matched)s, active=%(active)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class DriveRequest(Base):
+class DriveRequest(Base, ReprMixin):
     __tablename__ = 'drive_request'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -155,15 +124,8 @@ class DriveRequest(Base):
     passenger = relationship('Passenger', uselist=False, cascade='expunge',
                              primaryjoin="DriveRequest.passenger_id == Passenger.id")
 
-    def __repr__(self):
-        data = u'<DriveRequest id=%(id)s, driver_id=%(driver_id)s, '\
-                'passenger_id=%(passenger_id)s, '\
-                'accepted=%(accepted)s, active=%(active)s, '\
-                'cancelled=%(cancelled)s, response_time=%(response_time)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Rate(Base):
+class Rate(Base, ReprMixin):
     __tablename__ = 'rate'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -177,17 +139,8 @@ class Rate(Base):
     updated = Column(DateTime, default=datetime.utcnow,
                      onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        data = u'<Rating id=%(id)s, '\
-                'drive_request_id=%(drive_request_id)s, '\
-                'rater_user_id=%(rater_user_id)s, '\
-                'rated_user_id=%(rated_user_id)s, '\
-                'rater_is_driver=%(rater_is_driver)s, '\
-                'stars=%(stars)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Perk(Base):
+class Perk(Base, ReprMixin):
     __tablename__ = 'perk'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -202,23 +155,8 @@ class Perk(Base):
     per_seat_cost = Column(Float, nullable=False)
     per_distance_unit_cost = Column(Float, nullable=False)
 
-    def __repr__(self):
-        data = u''\
-            '<Perk '\
-            'id=%(id)s, '\
-            'drive_request_id=%(drive_request_id)s, '\
-            'deleted=%(deleted)s, '\
-            'name=%(name)s, '\
-            'duration=%(duration)s, '\
-            'fixed_rate=%(fixed_rate)s, '\
-            'multiplier=%(multiplier)s', \
-            'per_seat_cost=%(per_seat_cost)s', \
-            'per_distance_unit_cost=%(per_distance_unit_cost)s', \
-            '>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class DriverPerk(Base):
+class DriverPerk(Base, ReprMixin):
     __tablename__ = 'driver_perk'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -232,18 +170,8 @@ class DriverPerk(Base):
 
     perk = relationship('Perk', uselist=False, cascade='expunge')
 
-    def __repr__(self):
-        data = u''\
-            '<DriverPerk '\
-            'id=%(id)s, '\
-            'user_id=%(user_id)s, '\
-            'perk_id=%(perk_id)s, '\
-            'valid_until=%(valid_until)s, '\
-            '>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class PassengerPerk(Base):
+class PassengerPerk(Base, ReprMixin):
     __tablename__ = 'passenger_perk'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -257,18 +185,8 @@ class PassengerPerk(Base):
 
     perk = relationship('Perk', uselist=False, cascade='expunge')
 
-    def __repr__(self):
-        data = u''\
-            '<PassengerPerk '\
-            'id=%(id)s, '\
-            'user_id=%(user_id)s, '\
-            'perk_id=%(perk_id)s, '\
-            'valid_until=%(valid_until)s, '\
-            '>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Payment(Base):
+class Payment(Base, ReprMixin):
     __tablename__ = 'payment'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -281,17 +199,8 @@ class Payment(Base):
     payee_user_id = Column(String, ForeignKey('user.id'), nullable=True)
     credits = Column(Integer, nullable=False)
 
-    def __repr__(self):
-        data = u''\
-            '<Payment id=%(id)s, '\
-            'drive_request_id=%(drive_request_id)s, '\
-            'payer_user_id=%(payer_user_id)s, '\
-            'payee_user_id=%(payee_user_id)s, '\
-            'credits=%(credits)s>' % self.__dict__
-        return data.encode('utf-8')
 
-
-class Trace(Base):
+class Trace(Base, ReprMixin):
     __tablename__ = 'trace'
 
     id = Column(String, default=uuid, primary_key=True)
@@ -303,11 +212,3 @@ class Trace(Base):
     updated = Column(DateTime, default=datetime.utcnow,
                      onupdate=datetime.utcnow)
     user = relationship('User', uselist=False)
-
-    def __repr__(self):
-        data = u'<Trace id=%(id)s, user_id=%(user_id)s, '\
-                'level=%(level)s, date=%(date)s, '\
-                'message=%(message)s>' % self.__dict__
-        return data.encode('utf-8')
-
-
