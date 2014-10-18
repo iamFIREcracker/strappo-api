e650ec8c5e8ee43253e88400b0065140f3943ef9
Matteo Landi <matteo@matteolandi.net>
2014-10-17 23:54:11 -0400

    Change startup scripts to log an error message (and send an email) when an exception is caught

--- run_app.py
+++ run_app.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from app import app_factory
+import web
 
+from app import app_factory
+from app.weblib.logging import create_logger
+
+
+app = app_factory()
+
+
+def internalerror():
+    create_logger().exception('Holy shit!')
+    raise web.internalerror('Holy shit!')
+app.internalerror = internalerror
 
 if __name__ == '__main__':
-    app_factory().run()
+    app.run()
