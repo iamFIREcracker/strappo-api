81c98281a173688db8c2af5dc556707d445a671b
Matteo Landi <matteo@matteolandi.net>
2014-12-22 10:05:45 +0100

    Clean-up setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ def get_version():
                             shell=True,
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     pending, _ = proc.communicate()
-    return "%(tag)sd%(pending)s" % dict(tag='0.0.1', pending=pending)
+    return "%(pending)s" % dict(pending=pending)
 
 
 setup(name='strappo-api',
       version=get_version(),
       packages=['app'],
       install_requires=[
+          'gunicorn==19.1.1',
+          'weblib',
+          'strappon'
       ])
