555cc96a45dda72ab4fcf615fb990d45b3dab446
Matteo Landi <matteo@matteolandi.net>
2014-12-30 10:01:48 +0100

    indentation

--- app/forms/users.py
+++ app/forms/users.py
@@ -8,17 +8,15 @@ __all__ = ['add']
 
 
 validurl = form.Validator('Should be a valid URL.',
-                           lambda v: v.startswith('http://') or \
-                                     v.startswith('https://'))
+                          lambda v: v.startswith('http://') or
+                          v.startswith('https://'))
 
 
-add = form.Form(
-                form.Hidden('acs_id', form.notnull),
+add = form.Form(form.Hidden('acs_id', form.notnull),
                 form.Hidden('facebook_id', form.notnull),
                 form.Textbox('first_name', form.notnull),
                 form.Textbox('last_name', form.notnull),
                 form.Textbox('name', form.notnull),
                 form.Textbox('avatar', validurl),
                 form.Textbox('email', form.notnull),
-                form.Textbox('locale', form.notnull)
-)
+                form.Textbox('locale', form.notnull))
