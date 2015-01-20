22c0f126d420952cbaad1ce88cba0d9917223e44
Matteo Landi <matteo@matteolandi.net>
2015-01-20 10:35:09 -0500

    make integer_optional validator accept negative values

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -9,7 +9,7 @@ from web import form
 required = form.Validator('required', bool)
 integer = form.Validator('integer_required', bool)
 integer_optional = form.Validator('integer_optional',
-                                  lambda v: not v or int(v) >= 0)
+                                  lambda v: not v or int(v))
 
 
 def time_parser(v):
