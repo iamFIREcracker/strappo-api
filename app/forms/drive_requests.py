c4f8dce9436ca3dbe22ba21f54e487e87dd860cd
Matteo Landi <matteo@matteolandi.net>
2015-01-21 23:16:22 +0100

    Fix a problem with drive_request forms

--- app/forms/drive_requests.py
+++ app/forms/drive_requests.py
@@ -3,7 +3,7 @@
 
 from web import form
 
-from app.forms import datetime_validator
+from app.forms import datetime
 
 
-add = form.Form(form.Textbox('offered_pickup_time', datetime_validator))
+add = form.Form(form.Textbox('offered_pickup_time', datetime))
