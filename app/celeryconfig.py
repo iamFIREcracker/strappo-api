c2527472c4edae016b1fcec81984cbc1d7ebcafc
Matteo Landi <matteo@matteolandi.net>
2015-01-29 02:24:37 +0100

    Clean celeryconfig up

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
-
-from celery.schedules import crontab
-
 
 CELERY_IMPORTS = ('app.tasks',)
-
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-
-CELERYBEAT_SCHEDULE = {
-    'deactivate-expired-passengers': {
-        'task': 'app.tasks.DeactivateExpiredPassengersTask',
-        'schedule': crontab(minute='*/5')
-    }
-}
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
