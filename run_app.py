ce60a7db5806e446469f55af36a9a79f9366ab74
Matteo Landi <matteo@matteolandi.net>
2015-04-04 11:27:00 -0400

    update weblib and use default internalerror handler

--- run_app.py
+++ run_app.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import web
+from weblib import internalerror
 
 from app import app_factory
-from weblib.logging import create_logger
 
 
 app = app_factory()
+app.internalerror = internalerror('Holy shit!')
 
-
-def internalerror():
-    message = '''
-Holy shit!\n
-method: %(method)s
-fullpath: %(fullpath)s
-env: %(env)s
-''' % dict(method=web.ctx.method,
-           fullpath=web.ctx.fullpath,
-           env=web.ctx.environ
-           )
-    create_logger().exception(message)
-    raise web.internalerror('Holy shit!')
-app.internalerror = internalerror
 
 if __name__ == '__main__':
     app.run()
