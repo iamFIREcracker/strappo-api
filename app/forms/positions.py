8b86b0a6401fb2e1e0ad73b12f78d68010e3d7eb
Matteo Landi <matteo@matteolandi.net>
2015-05-17 10:59:43 -0400

    Add missing positions form file

---
+++ app/forms/positions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from web import form
+
+from app.forms import required
+
+
+add = form.Form(form.Textbox('latitude', required),
+                form.Textbox('longitude', required))
